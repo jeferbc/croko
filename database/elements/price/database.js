0499a50cb94a3627a56d026d6f9b4be6fcde9b8f
jeferbc <jeffe.bernal@gmail.com>
2023-07-17 13:37:02 -0500

    Adjust price

--- database/elements/price/database.js
+++ database/elements/price/database.js
@@ -5,14 +5,14 @@ module.exports = {
             feature1: "Diseño personalizado",
             feature2: 'Servicio en estudio (Itagüí)',
             feature3: '',
-            price: '220.000',
+            price: '170.000',
         },
         {
             title: "Básico",
             feature1: "Diseño personalizado",
             feature2: 'Servicio a domicilio (Medellín)',
             feature3: '',
-            price: '300.000',
+            price: '250.000',
         },
         {
             title: "Integrado",
